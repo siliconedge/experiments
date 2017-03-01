87d34a518c4de95b7196e0acb54a8ccf1b58200a
Michael Routitski <MRoutitski@bluedoor.local>
2017-03-02 09:31:16 +1100

    init

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine;
+using System.Linq;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
